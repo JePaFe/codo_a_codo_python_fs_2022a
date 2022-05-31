be03677470c11b054200a08486ba3d074ffe417d
Jean Paul Ferreira <jeanpaul@jepafe.net>
2022-05-30 23:38:38 -0300

    Clase 23 - Python

---
+++ Clase 23 Python/input.py
@@ -0,0 +1,10 @@
+# nombre = input('Ingrese su nombre: ')
+# print('Hola ' + nombre)
+
+num1 = 3
+num2 = input('Ingrese un numero: ')
+
+print(type(num1), type(num2))
+
+#num2 = int(float(input('Ingrese un numero: ')))
+#print(num1 + num2)
---
+++ Clase 23 Python/string.py
@@ -0,0 +1,6 @@
+mensaje = 'hola Mundo'
+
+print(mensaje[6], len(mensaje))
+print(mensaje.upper(), type(mensaje))
+print(mensaje.capitalize())
+print(mensaje.__len__())
